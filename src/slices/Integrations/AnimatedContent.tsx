6bfc1aab5cfb8f6b93990cb1b4b3b925d895675d
divine <divineosuuweb@gmail.com>
2024-08-06 12:11:02 +0200

    ensure all hooks are called unconditionally

--- src/slices/Integrations/AnimatedContent.tsx
+++ src/slices/Integrations/AnimatedContent.tsx
@@ -1,14 +1,12 @@
 "use client";
-import React from 'react';
+import React, { useRef } from 'react';
 import StylizedLogoMark from './StylizedLogoMark';
 import clsx from 'clsx';
 import { Content } from '@prismicio/client';
-import { useRef } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import usePrefersReducedMotion from "@/hooks/usePreferedReducedMotion"
+import usePrefersReducedMotion from "@/hooks/usePreferedReducedMotion";
 import { FaDigitalOcean, FaCloudflare, FaNpm, FaGithub, FaFigma, FaFly } from "react-icons/fa6";
-
 
 export default function AnimatedContent({ slice }: { slice: Content.IntegrationsSlice }) {
 
@@ -19,22 +17,21 @@ export default function AnimatedContent({ slice }: { slice: Content.Integrations
         figma: <FaFigma />,
         digitalocean: <FaDigitalOcean />,
         fly: <FaFly />
-    }
-
+    };
 
     const container = useRef(null);
-    const preferesReducedMotion = usePrefersReducedMotion();
+    const prefersReducedMotion = usePrefersReducedMotion();
     gsap.registerPlugin(useGSAP);
 
-    if(preferesReducedMotion){
-        return ;
-      }
-
     useGSAP(() => {
+        if (prefersReducedMotion) {
+            return;
+        }
+
         const tl = gsap.timeline({
             repeat: -1,
             defaults: { ease: "power2.inOut" }
-        })
+        });
 
         tl.to(".pulsing-logo", {
             keyframes: [
@@ -46,11 +43,11 @@ export default function AnimatedContent({ slice }: { slice: Content.Integrations
                 },
                 {
                     filter: "brightness(1)",
-                    opacity: .7,
+                    opacity: 0.7,
                     duration: 0.9,
                 }
             ]
-        })
+        });
 
         tl.to(".signal-line", {
             keyframes: [
@@ -62,11 +59,8 @@ export default function AnimatedContent({ slice }: { slice: Content.Integrations
                         duration: 1
                     }
                 },
-
-
             ]
-        }, "-=1.4")
-
+        }, "-=1.4");
 
         tl.to(".pulsin-icon", {
             keyframes: [
@@ -87,29 +81,28 @@ export default function AnimatedContent({ slice }: { slice: Content.Integrations
                     duration: 1
                 }
             ]
-        }, "-=2")
-
-    }, { scope: container })
+        }, "-=2");
+    }, { scope: container });
 
     return (
         <div ref={container} className="mt-20 flex flex-col items-center md:flex-row">
             {slice.items.map((item, index) => (
                 <React.Fragment key={index}>
-                    {index == Math.floor(slice.items.length / 2) && (
+                    {index === Math.floor(slice.items.length / 2) && (
                         <>
                             <StylizedLogoMark />
                             <div className="signal-line rotate-180 bg-gradient-to-t" />
                         </>
                     )}
                     <div className="pulsin-icon flex aspect-square shrink-0 items-center justify-center rounded-full
-              border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 md:text-4xl lg:text-5xl">
+                        border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 md:text-4xl lg:text-5xl">
                         {item.icon && icons[item.icon]}
                     </div>
-                    {index != slice.items.length - 1 &&
+                    {index !== slice.items.length - 1 &&
                         <div className={clsx("signal-line", index >= Math.floor(slice.items.length / 2) ? "rotate-180" : "rotate-0")} />
                     }
                 </React.Fragment>
             ))}
         </div>
-    )
+    );
 }
